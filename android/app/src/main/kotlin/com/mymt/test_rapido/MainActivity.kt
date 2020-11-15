337a670c6fec7170a1f050980010e360ddeb431c
mose <kk.flap.gg@gmail.com>
2020-11-15 17:55:13 +0900

    rapidoパッケージexample

---
+++ android/app/src/main/kotlin/com/mymt/test_rapido/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mymt.test_rapido
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:rapido/rapido.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rapido Demo',
+      home: RapidoExample(title: 'Rapdio'),
+    );
+  }
+}
+
+class RapidoExample extends StatefulWidget {
+  RapidoExample({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _RapidoExampleState createState() => _RapidoExampleState();
+}
+
+class _RapidoExampleState extends State<RapidoExample> {
+  // Create a Document with a documentType string of your choice.
+  // Include a map of lables for the fields in the document type.
+  final DocumentList documentList = DocumentList(
+    "task list",
+    labels: {
+      "Complete": "done?",
+      "Date": "date",
+      "Title": "title",
+      "Note": "subtitle",
+      "Priorty": "pri count"
+    },
+  );
+
+  // Build a DocumentListScaffold to provide the UI for users to
+  // create, edit, and delete documents
+  @override
+  Widget build(BuildContext context) {
+    return DocumentListScaffold(
+      documentList,
+      title: "Tasks",
+    );
+  }
+}
